490801c63ecbe500a45e8840bb3b3a7ea661c1d2
“MUbarak123-56” <mubarak.a.ganiyu@vanderbilt.edu>
2023-12-16 09:56:06 -0600

    new

--- telecom_churn/vqc_code/vqc_pauli.py
+++ telecom_churn/vqc_code/vqc_pauli.py
@@ -51,7 +51,7 @@ from qiskit.circuit import ParameterVector, Parameter
 pauli_feature_map = PauliFeatureMap(feature_dimension=len(X_train.columns),reps=1, paulis=['ZZ'])
 
 ### Ansatzes
-ansatz_su = EfficientSU2(num_qubits=pauli_feature_map.width(), reps = 2, su2_gates=["ry", "rz"], entanglement= "full",
+ansatz_su = EfficientSU2(num_qubits=pauli_feature_map.width(), reps = 3, su2_gates=["ry", "rz"], entanglement= "full",
                          insert_barriers=True)
 ansatz_two_local = TwoLocal(num_qubits=pauli_feature_map.width(),rotation_blocks=["ry", "rz"],entanglement_blocks="cx",
                                      entanglement="linear", reps=2, insert_barriers=True)
@@ -65,15 +65,16 @@ ansatz_n_local = NLocal(num_qubits=pauli_feature_map.width(),rotation_blocks=[RX
 num_iter=300
 cobyla = COBYLA(maxiter = num_iter)
 spsa = SPSA(maxiter = num_iter)
+reps = 3
 
 def plot_confusion_matrix(conf_matrix, ansatz, optimizer):
     #num = len(os.listdir("../vqc_conf/train_"))
-    conf_num = "../vqc_conf/train_process/" + str(ansatz) + "_" + str(optimizer) + ".png"
+    conf_num = "../vqc_conf/train_process/" + str(ansatz) + "_" + str(optimizer) + "_" + str(reps) + ".png"
     plt.figure(figsize=(8, 6))
     sns.set(font_scale=1.2)
     sns.heatmap(conf_matrix, annot=True, fmt='g', cmap='Blues', cbar=False,
-                xticklabels=['Predicted 0', 'Predicted 1', 'Predicted 2'],
-                yticklabels=['Actual 0', 'Actual 1', 'Actual 2'])
+                xticklabels=['Predicted 0', 'Predicted 1'],
+                yticklabels=['Actual 0', 'Actual 1'])
     plt.xlabel('Predicted Labels')
     plt.ylabel('True Labels')
     plt.title('Confusion Matrix')
@@ -117,7 +118,7 @@ def vqc_exp(ansatz, optimizer):
     elapsed = time.time() - start
 
     #num = len(os.listdir("../vqc_model/pauli"))
-    vqc_mod_num = "../vqc_model/train_process/" + str(ansatz) + "_" + str(optimizer) + ".model"
+    vqc_mod_num = "../vqc_model/train_process/" + str(ansatz) + "_" + str(optimizer) + "_" + str(reps) + ".model"
     vqc.save(vqc_mod_num)
 
     print(f"Training time: {round(elapsed)} seconds")
@@ -153,11 +154,12 @@ def vqc_exp(ansatz, optimizer):
     df["Training time"] = elapsed
     df["Model"] = "VQC"
     df["Max Iter"] = num_iter
+    df["reps"] = reps
 
     df = df.drop("one", axis = 1)
 
     #num = len(os.listdir("../vqc_results/pauli"))
-    df.to_csv("../vqc_results/train_process/" + str(ansatz) + "_" + str(optimizer)  + ".csv", index = False)
+    df.to_csv("../vqc_results/train_process/" + str(ansatz) + "_" + str(optimizer)  + "_" + str(reps) + ".csv", index = False)
     # clear objective value history
     #objective_func_vals = []
     #return df
---
+++ telecom_churn/vqc_code/vqc_pauli_best.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from qiskit import *
+from qiskit_machine_learning.algorithms.classifiers import VQC
+from qiskit.circuit.library import EfficientSU2, TwoLocal, NLocal, RealAmplitudes, ZFeatureMap, ZZFeatureMap, PauliFeatureMap
+from qiskit.circuit.library import CCXGate, CRZGate, RXGate
+from qiskit.algorithms.optimizers import COBYLA, SLSQP, SPSA
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
+from qiskit.primitives import Sampler
+from qiskit.circuit import ParameterVector
+from sklearn.metrics import recall_score, precision_score, f1_score
+import time
+from qiskit_machine_learning.algorithms.classifiers import VQC
+import os
+
+train = pd.read_csv("../data/train_fe_small.csv")
+test = pd.read_csv("../data/test_fe.csv")
+
+cols = ['region_South', 'region_West', 'account_length','number_vmail_messages', 'total_day_minutes', 'total_day_calls',
+        'total_intl_charge', 'customer_service_calls', 'churn']
+
+train = train[cols]
+
+x_train_use, y_train_use = train.drop("churn", axis = 1), train["churn"]
+
+test = test[cols]
+
+x_test, y_test = test.drop("churn", axis =1), test["churn"]
+
+pauli_feature_map = PauliFeatureMap(feature_dimension=len(x_train_use.columns), reps=1)
+sampler = Sampler()
+ansatz_su = EfficientSU2(num_qubits=pauli_feature_map.width(), reps = 2, su2_gates=["ry", "rz"], entanglement= "full",
+                         insert_barriers=True)
+num_iter=300
+cobyla = COBYLA(maxiter = num_iter)
+model = VQC(
+        sampler=sampler,
+        feature_map=pauli_feature_map,
+        ansatz=ansatz_su,
+        optimizer=cobyla)
+
+#model.load("../vqc_model/train_process/su2_cobyla.model")
+
+x_train_arr = x_train_use.to_numpy()
+y_train_arr = y_train_use.to_numpy()
+x_test_arr = x_test.to_numpy()
+y_test_arr = y_test.to_numpy()
+
+model.fit(x_train_arr, y_train_arr)
+
+pred_use = model.predict(x_train_arr)
+pred_test = model.predict(x_test_arr)
+
+f1_train = f1_score(y_train_use, pred_use)
+prec_train = precision_score(y_train_use, pred_use)
+recall_train = recall_score(y_train_use, pred_use)
+
+f1_test = f1_score(y_test, pred_test)
+prec_test = precision_score(y_test, pred_test)
+recall_test = recall_score(y_test, pred_test)
+
+df = pd.DataFrame()
+df["f1_test"] = [f1_test]
+df["f1_train"] = f1_train
+df["prec_train"] = prec_train
+df["prec_test"] = prec_test
+df["recall_train"] = recall_train
+df["recall_test"] = recall_test
+df["model"] = "VQC_best"
+
+df.to_csv("../vqc_results/final/vqc.csv", index=False)
