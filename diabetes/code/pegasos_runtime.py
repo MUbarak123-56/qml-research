afe41844d6ca4e4c3cfa4233070bb36479fd8cc5
“MUbarak123-56” <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-16 17:38:30 -0600

    new

--- diabetes/code/pegasos_runtime.py
+++ diabetes/code/pegasos_runtime.py
@@ -64,7 +64,7 @@ for i in range(1,len(cols)):
     qkernel = FidelityQuantumKernel(feature_map=feature_map)
 
     # Initialize the QSVC model
-    model = PegasosQSVC(quantum_kernel=qkernel, C=750, num_steps=300)
+    model = PegasosQSVC(quantum_kernel=qkernel, C=200, num_steps=300)
     start = time.time()
     model.fit(new_x, np.array(y_train))
     stop = time.time()
